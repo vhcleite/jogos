11558ebd2b40f3682ddd0b7d17e7d3229678cb40
Victor Leite <victor_leite@outlook.com.br>
2018-02-10 13:02:57 -0200

    commit depois de usar git add -i

--- jogos.py
+++ jogos.py
@@ -1,8 +1,9 @@
 import advinhacao
 import forca
+import zelda
 
 def iniciar_jogo():
-    print("Escolha o jogo\n(1) Advinhação\n(2) Forca")
+    print("Escolha o jogo\n(1) Advinhação\n(2) Forca\n(3) Zelda")
 
     escolha = int(input("Escolha: "))
     print("Você escolheu ", escolha)
@@ -11,6 +12,9 @@ def iniciar_jogo():
         advinhacao.jogar()
     elif(escolha == 2):
         forca.jogar()
+    elif(escolha == 3):
+    	zelda.jogar()
 
 if(__name__ == "__main__"):
     iniciar_jogo()
+    #testando o git add -i
