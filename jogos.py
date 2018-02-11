15f796463a7a3a60c753f8e517338b7d9df14100
Victor Leite <victor_leite@outlook.com.br>
2018-02-11 11:43:43 -0200

    Mudado layout de escolha de jogo

--- jogos.py
+++ jogos.py
@@ -4,9 +4,16 @@ import zelda
 import mario
 
 def iniciar_jogo():
-    print("Escolha o jogo\n(1) Advinhação\n(2) Forca\n(3) Zelda\n(4) Mario")
+    #print("Escolha o jogo\n(1) Advinhação\n(2) Forca\n(3) Zelda\n(4) Mario")
+    print("###################")
+    print("Escolha um jogo: ")
+    print("1 - Advinhação")
+    print("2 - Forca")
+    print("3 - Zelda")
+    print("4 - Mario")
+    print("###################")
 
-    escolha = int(input("Escolha: "))
+    escolha = int(input("Número do jogo: "))
     print("Você escolheu ", escolha)
 
     if (escolha == 1):
