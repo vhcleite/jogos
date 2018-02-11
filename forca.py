41cf455083302c8b3af44c8265c3d0e8ef79cf6a
Victor Leite <victor_leite@outlook.com.br>
2018-02-11 12:49:53 -0200

    adicionadas variáveis booleanas

--- forca.py
+++ forca.py
@@ -4,6 +4,15 @@ def jogar():
     print("Bem vindo ao jogo da forca")
     print("*******************************")
 
+
+    palavra_secreta = "cachorro"
+
+    acertou = False
+    enforcou = False
+
+    while(not acertou and not enforcou):
+        print("Jogando...")
+
     print("*** Fim de Jogo ***")
 
 if(__name__ == "__main__"):
