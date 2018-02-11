1e617517ec0c25705c21c9aa2f5c554def0b3fb7
Victor Leite <victor_leite@outlook.com.br>
2018-02-11 09:09:25 -0200

    Adicionado jogo de Mario

--- jogos.py
+++ jogos.py
@@ -1,9 +1,10 @@
 import advinhacao
 import forca
 import zelda
+import mario
 
 def iniciar_jogo():
-    print("Escolha o jogo\n(1) Advinhação\n(2) Forca\n(3) Zelda")
+    print("Escolha o jogo\n(1) Advinhação\n(2) Forca\n(3) Zelda\n(4) Mario")
 
     escolha = int(input("Escolha: "))
     print("Você escolheu ", escolha)
@@ -14,6 +15,8 @@ def iniciar_jogo():
         forca.jogar()
     elif(escolha == 3):
     	zelda.jogar()
+    elif(escolha == 4):
+    	mario.jogar()
 
 if(__name__ == "__main__"):
     iniciar_jogo()
