2f78e6ff879d7776e1e20a1c4fc5b2fa17d8ff70
Victor Leite <victor_leite@outlook.com.br>
2018-02-12 12:54:43 -0200

    Condições para encerramento do jogo implementado

--- forca.py
+++ forca.py
@@ -5,19 +5,35 @@ def jogar():
     print("*******************************")
 
 
-    palavra_secreta = "cachorro"
-
+    palavra_secreta = "cachorro".upper()
+    palavra_acertada = ["_", "_", "_", "_", "_", "_", "_", "_"]
     acertou = False
     enforcou = False
+    tentativas = 6
 
+    print(palavra_acertada)
     while(not acertou and not enforcou):
         chute = input("Digite uma letra: ")
-        chute = chute.strip()
+        chute = chute.strip().upper()
         index = 0
-        for letra in palavra_secreta:
-            if letra.upper() == chute.upper():
-                print("Encoutrou '{}' na posição '{}'".format(letra, index))
-            index = index + 1
+        if(chute in palavra_secreta):
+            for letra in palavra_secreta:
+                if letra == chute:
+                    palavra_acertada[index] = letra
+                    print("Encoutrou '{}' na posição '{}'".format(letra, index))
+                index += 1
+        else:
+            tentativas -= 1
+            print("Faltam {} tentativas".format(tentativas))
+            if ((tentativas == 0)):
+                enforcou = True
+                print("ENFORCOU")
+
+        if (palavra_acertada.count("_") == 0):
+            print("Você VENCEU  ")
+            acertou = True
+
+        print(palavra_acertada)
 
     print("*** Fim de Jogo ***")
 
