cbf40bef4e14a0940e70cc8e5cd7a10cea3c2841
Victor Leite <victor_leite@outlook.com.br>
2018-02-12 15:31:32 -0200

    Forca: 	Escolha aleatórioa de palavra secreta; 	criação de lista com possíveis palavras; 	Código organizado em funções em funções; 	Finalização do código.

--- forca.py
+++ forca.py
@@ -1,27 +1,60 @@
+import random
 
-def jogar():
+
+def mostrar_mensagem_abertura():
     print("*******************************")
     print("Bem vindo ao jogo da forca")
     print("*******************************")
 
 
-    palavra_secreta = "cachorro".upper()
-    palavra_acertada = ["_", "_", "_", "_", "_", "_", "_", "_"]
+def escolher_palavra_secreta():
+    arquivo = open("palavras.txt", "r")
+    palavras = []
+    for line in arquivo:
+        line = line.strip()
+        palavras.append(line)
+
+    arquivo.close()
+
+    numero_aleatorio = random.randrange(0, len(palavras))
+    palavra_secreta = palavras[numero_aleatorio].upper()
+    return palavra_secreta
+
+
+def inicializar_palavra_acertada(palavra_secreta):
+    palavra_acertada = ["_" for letra in palavra_secreta]  # utilizando list comprehensions
+    return palavra_acertada
+
+
+def pegar_chute_jogador():
+    chute = input("Digite uma letra: ")
+    chute = chute.strip().upper()
+    return chute
+
+
+def encontrar_letras_na_palvra(palavra_secreta, chute, palavra_acertada):
+    index = 0
+    for letra in palavra_secreta:
+        if letra == chute:
+            palavra_acertada[index] = letra
+        index += 1
+
+
+def jogar():
+    mostrar_mensagem_abertura()
+    palavra_secreta = escolher_palavra_secreta()
+    palavra_acertada = inicializar_palavra_acertada(palavra_secreta)
+    print(palavra_acertada)
+
     acertou = False
     enforcou = False
     tentativas = 6
 
-    print(palavra_acertada)
     while(not acertou and not enforcou):
-        chute = input("Digite uma letra: ")
-        chute = chute.strip().upper()
-        index = 0
+        chute = pegar_chute_jogador()
+
         if(chute in palavra_secreta):
-            for letra in palavra_secreta:
-                if letra == chute:
-                    palavra_acertada[index] = letra
-                    print("Encoutrou '{}' na posição '{}'".format(letra, index))
-                index += 1
+            encontrar_letras_na_palvra(palavra_secreta, chute, palavra_acertada)
         else:
             tentativas -= 1
             print("Faltam {} tentativas".format(tentativas))
